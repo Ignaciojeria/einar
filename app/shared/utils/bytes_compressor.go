1ae960f2e8b1e74b1d790ced70e9f8fbae5b61c3
Ignacio <ignaciovl.j@gmail.com>
2023-05-13 16:17:58 -0400

    Update bytes_compressor.go

--- app/shared/utils/bytes_compressor.go
+++ app/shared/utils/bytes_compressor.go
@@ -18,7 +18,7 @@ func CompressBytes(s []byte) []byte {
 func DecompressBytes(s []byte) ([]byte, error) {
 	rdr, err := gzip.NewReader(bytes.NewReader(s))
 
-	customError := errors.New("error descompresing bytes")
+	customError := errors.New("error decompresing bytes")
 
 	if err != nil {
 		return nil, customError
