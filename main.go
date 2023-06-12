884969372e050847e49e5bd0bc491f69b4028a07
Ignacio <ignaciovl.j@gmail.com>
2023-06-12 03:31:18 +0000

    testing tags generation

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"dagger.io/dagger"
 	"github.com/joho/godotenv"
 )
-var version = "1.0.5"
+var version = "1.0.0"
 
 func main() {
 	err := godotenv.Load()
--- pipelines/2_set_release_tag.go
+++ pipelines/2_set_release_tag.go
@@ -8,42 +8,51 @@ import (
 )
 
 func SetReleaseTag(ctx context.Context, tagName, tagMessage string) error {
-	// Create a new OAuth2 client using the GitHub access token
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")})
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")},
+	)
 	tc := oauth2.NewClient(ctx, ts)
+
 	client := github.NewClient(tc)
 
-	// Get the reference to the current commit of the branch
-	ref, _, err := client.Git.GetRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), "refs/heads/"+os.Getenv("GITHUB_BRANCH"))
+	ref, _, err := client.Git.GetRef(
+		ctx, 
+		os.Getenv("GITHUB_OWNER"), 
+		os.Getenv("GITHUB_REPO"), 
+		"refs/heads/"+os.Getenv("GITHUB_BRANCH"))
 	if err != nil {
 		return err
 	}
 
-	// Create a new git tag and associate it with the current commit
+	// Create new tag
 	tag := &github.Tag{
 		Tag:     github.String(tagName),
 		Message: github.String(tagMessage),
 		Object:  &github.GitObject{SHA: ref.Object.SHA, Type: github.String("commit")},
 	}
+
 	tag, _, err = client.Git.CreateTag(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), tag)
 	if err != nil {
 		return err
 	}
 
-	// Create a reference to the new tag
 	refObj := github.Reference{
 		Ref:    github.String("refs/tags/" + *tag.Tag),
 		Object: &github.GitObject{SHA: tag.SHA, Type: github.String("tag")},
 	}
+
 	_, _, err = client.Git.CreateRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), &refObj)
 	if err != nil {
 		return err
 	}
 
-	// Update the branch to point to the new commit
-	refObj.Ref = github.String("refs/heads/" + os.Getenv("GITHUB_BRANCH"))
-	refObj.Object.SHA = tag.SHA
-	_, _, err = client.Git.UpdateRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), &refObj, false)
+	// Update "latest" tag to point to same commit as new tag
+	latestRefObj := github.Reference{
+		Ref:    github.String("refs/tags/latest"),
+		Object: &github.GitObject{SHA: tag.SHA, Type: github.String("tag")},
+	}
+
+	_, _, err = client.Git.UpdateRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), &latestRefObj, true)
 	if err != nil {
 		return err
 	}
