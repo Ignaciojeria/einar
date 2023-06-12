e6d80f9e00b2e9500ee30c806320054e6dc629be
Ignacio <ignaciovl.j@gmail.com>
2023-06-12 03:26:15 +0000

    testing setReleaseTag

--- pipelines/2_set_release_tag.go
+++ pipelines/2_set_release_tag.go
@@ -6,43 +6,47 @@ import (
 	"github.com/google/go-github/v39/github"
 	"golang.org/x/oauth2"
 )
-func SetReleaseTag(ctx context.Context, tagName, tagMessage string) error {
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
 
+func SetReleaseTag(ctx context.Context, tagName, tagMessage string) error {
+	// Create a new OAuth2 client using the GitHub access token
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")})
+	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	ref, _, err := client.Git.GetRef(
-		ctx, 
-		os.Getenv("GITHUB_OWNER"), 
-		os.Getenv("GITHUB_REPO"), 
-		"refs/heads/"+os.Getenv("GITHUB_BRANCH"))
+	// Get the reference to the current commit of the branch
+	ref, _, err := client.Git.GetRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), "refs/heads/"+os.Getenv("GITHUB_BRANCH"))
 	if err != nil {
 		return err
 	}
 
+	// Create a new git tag and associate it with the current commit
 	tag := &github.Tag{
 		Tag:     github.String(tagName),
 		Message: github.String(tagMessage),
 		Object:  &github.GitObject{SHA: ref.Object.SHA, Type: github.String("commit")},
 	}
-
 	tag, _, err = client.Git.CreateTag(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), tag)
 	if err != nil {
 		return err
 	}
 
+	// Create a reference to the new tag
 	refObj := github.Reference{
 		Ref:    github.String("refs/tags/" + *tag.Tag),
 		Object: &github.GitObject{SHA: tag.SHA, Type: github.String("tag")},
 	}
-
 	_, _, err = client.Git.CreateRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), &refObj)
 	if err != nil {
 		return err
 	}
-	
+
+	// Update the branch to point to the new commit
+	refObj.Ref = github.String("refs/heads/" + os.Getenv("GITHUB_BRANCH"))
+	refObj.Object.SHA = tag.SHA
+	_, _, err = client.Git.UpdateRef(ctx, os.Getenv("GITHUB_OWNER"), os.Getenv("GITHUB_REPO"), &refObj, false)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
